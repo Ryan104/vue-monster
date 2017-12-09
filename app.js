169e2711b89966216e1cf223934b55fa761f86fd
Ryan <ryan.d.elliott@gmail.com>
2017-12-09 14:04:18 -0700

    Display name and health

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log('app.js running')
+
+new Vue({
+  el: '#app',
+  data: {
+    characters: {
+      player: {
+        name: 'Player',
+        healthRemaining: 100,
+        maxHealth: 100,
+      },
+      monster: {
+        name: 'Monster',
+        healthRemaining: 100,
+        maxHealth: 100,
+      }
+    }
+  },
+  methods: {
+    takeDamage: function(character, amount){
+      this.characters[character].healthRemaining -= amount
+    },
+    healPlayer: function(amount){
+      this.characters.player.healthRemaining += amount
+    }
+  }
+})
