4688c397b2b21a434e2d3e3e646be0ace5e2a687
Ryan <ryan.d.elliott@gmail.com>
2017-12-09 15:01:00 -0700

    Calc attack value and add pictures

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ console.log('app.js running')
 new Vue({
   el: '#app',
   data: {
+    playing: false,
     characters: {
       player: {
         name: 'Player',
         healthRemaining: 100,
         maxHealth: 100,
+        attackPower: 20,
+        imageSrc: './knight.png'
       },
       monster: {
         name: 'Monster',
         healthRemaining: 100,
         maxHealth: 100,
+        attackPower: 30,
+        imageSrc: './monster.png'
       }
     }
   },
@@ -22,6 +27,19 @@ new Vue({
     },
     healPlayer: function(amount){
       this.characters.player.healthRemaining += amount
+    },
+    surrender: function(){
+      this.playing = false
+    },
+    calculateAttackPoints: function(attackPower){
+      return Math.round(Math.random() * attackPower)
+    },
+    handleAttack: function(){
+      let monsterAttack = this.calculateAttackPoints(this.characters.monster.attackPower)
+      let playerAttack = this.calculateAttackPoints(this.characters.player.attackPower)
     }
+  },
+  calculated: {
+    
   }
 })
